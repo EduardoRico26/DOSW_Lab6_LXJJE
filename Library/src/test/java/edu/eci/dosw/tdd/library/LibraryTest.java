64d34934c06ed59ef1a5ad415a25d0c4af508bc4
Juan Roa <juan.roa-h@mail.escuelaing.edu.co>
2026-03-11 09:08:21 -0500

    test verificar libro dentro del sistema

--- Library/src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
+++ Library/src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
@@ -3,5 +3,38 @@ package edu.eci.dosw.tdd.library;
 import org.junit.jupiter.api.Test;
 
 public class LibraryTest {
+    @Test
+    public void shouldAddBookAndKeepItInSystem() {
+        Library library = new Library();
+        Book book = new Book("Pedro Páramo", "Juan Rulfo", "666");
+        library.addBook(book);
+        // Al prestarlo exitosamente confirma que sí quedó registrado
+        User user = new User();
+        user.setId("5");
+        library.addUser(user);
+        Loan loan = library.loanABook("5", "666");
+        assertNotNull(loan);
+    }
+
+    @Test
+    public void shouldLoanBookWithActiveDateSet() {
+        Library library = new Library();
+        Book book = new Book("Ficciones", "Jorge Luis Borges", "777");
+        library.addBook(book);
+        User user = new User();
+        user.setId("5");
+        library.addUser(user);
+        Loan loan = library.loanABook("5", "777");
+        assertNotNull(loan.getLoanDate());
+    }
+
+    @Test
+    public void shouldReturnNullWhenLoanNotInSystem() {
+        Library library = new Library();
+        // Se crea un préstamo manualmente sin pasar por loanABook
+        Loan fakeLoan = new Loan();
+        Loan result = library.returnLoan(fakeLoan);
+        assertNull(result);
+    }
 
 }
