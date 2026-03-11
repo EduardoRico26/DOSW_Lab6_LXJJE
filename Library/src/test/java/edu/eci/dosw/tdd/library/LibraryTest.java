bd1f9a8f68f363645540c7bdc576d522f91ae410
Eduardo Rico Duarte <edurico0621@gmail.com>
2026-03-10 23:03:58 -0500

    Corrección encabezado

--- Library/src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
+++ Library/src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
@@ -1,6 +1,12 @@
 package edu.eci.dosw.tdd.library;
 
+import edu.eci.dosw.tdd.library.book.Book;
+import edu.eci.dosw.tdd.library.loan.Loan;
+import edu.eci.dosw.tdd.library.loan.LoanStatus;
+import edu.eci.dosw.tdd.library.user.User;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LibraryTest {
 
