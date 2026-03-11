668cfce63eec66d8a5c9e8c6731e2cf8f15e64cd
Eduardo Rico Duarte <edurico0621@gmail.com>
2026-03-10 22:59:28 -0500

    Caso de prueba para el método AddBook

--- Library/src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
+++ Library/src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
@@ -1,7 +1,24 @@
 package edu.eci.dosw.tdd.library;
 
+import edu.eci.dosw.tdd.library.book.Book;
+import edu.eci.dosw.tdd.library.loan.Loan;
+import edu.eci.dosw.tdd.library.loan.LoanStatus;
+import edu.eci.dosw.tdd.library.user.User;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LibraryTest {
+
+    @Test
+    public void shouldAddABook(){
+
+        Library library = new Library();
+        Book book = new Book("Cien años de soledad","Gabriel García Márquez","111");
+
+        boolean result = library.addBook(book);
+
+        assertTrue(result);
+    }
 
 }
